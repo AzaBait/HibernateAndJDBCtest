e5f6fec41484c7b5db1c4bf3668153e32233e481
dell <m>
2021-09-25 18:28:22 +0600

    added to Main class

--- JDBCAndHibernateTask-master/jdbc/jdbcAndHibernate/src/main/java/peaksoft/Main.java
+++ JDBCAndHibernateTask-master/jdbc/jdbcAndHibernate/src/main/java/peaksoft/Main.java
@@ -1,9 +1,18 @@
 package peaksoft;
 
+import peaksoft.dao.UserDao;
+import peaksoft.dao.UserDaoJdbcImpl;
 import peaksoft.util.Util;
 
 public class Main {
     public static void main(String[] args) {
-
+        UserDao userDaoJDBC = new UserDaoJdbcImpl();
+       userDaoJDBC.createUsersTable();
+        userDaoJDBC.saveUser("Azamat", "Baitashov", (byte) 35);
+        userDaoJDBC.saveUser("Kuban", "Nurdinov", (byte) 35);
+        userDaoJDBC.getAllUsers();
+        userDaoJDBC.removeUserById(2);
+        userDaoJDBC.cleanUsersTable();
+        userDaoJDBC.dropUsersTable();
     }
 }
